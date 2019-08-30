f7e142ca73c0fc98a4fb362cac11277e45e5381f
Bryndon Lee <bryndontlee.git@gmail.com>
2019-08-29 22:17:32 -0500

    Complete Exercise 1-12.

---
+++ Chapter1/1_5_i.c
@@ -0,0 +1,26 @@
+/* Exercise 1-12: Write a program that prints its input one word per line. */
+# include <stdio.h>
+
+# define IN 1   // Inside a word.
+# define OUT 0  // Outside a word.
+
+main()
+{
+  int c, state;
+
+  state = OUT;
+  while ((c = getchar()) != EOF)
+  {
+    if (c == ' ' || c == '\t') // Not yet sure how to implement \n.
+    {
+      if (state == IN)
+        printf("\n");
+      state = OUT;
+    }
+    else
+    {
+      state = IN;
+      putchar(c);
+    }
+  }
+}
